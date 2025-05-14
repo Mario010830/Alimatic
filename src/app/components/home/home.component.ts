452a1099334dba7b983ad5f16b6d42cd90a57f5a
Mario <mario.ortega@ntsprint.com>
2025-05-14 10:04:51 -0600

    Changes 140525

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,39 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('provideText') provideText!: ElementRef;
+  @ViewChild('provideImages') provideImages!: ElementRef;
 
+  ngAfterViewInit() {
+    if (typeof window !== 'undefined' && 'IntersectionObserver' in window) {
+      if (this.provideText && this.provideImages) {
+        const options = {
+          root: null,
+          rootMargin: '0px',
+          threshold: 0.2
+        };
+        const observer = new window.IntersectionObserver((entries) => {
+          entries.forEach(entry => {
+            if (entry.target === this.provideText.nativeElement) {
+              if (entry.isIntersecting) {
+                this.provideText.nativeElement.classList.add('animate-left');
+              } else {
+                this.provideText.nativeElement.classList.remove('animate-left');
+              }
+            }
+            if (entry.target === this.provideImages.nativeElement) {
+              if (entry.isIntersecting) {
+                this.provideImages.nativeElement.classList.add('animate-right');
+              } else {
+                this.provideImages.nativeElement.classList.remove('animate-right');
+              }
+            }
+          });
+        }, options);
+        observer.observe(this.provideText.nativeElement);
+        observer.observe(this.provideImages.nativeElement);
+      }
+    }
+  }
 }
